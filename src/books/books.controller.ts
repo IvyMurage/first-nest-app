50b412283689706b7c702c05c938e12a31d616db
Murage-Ivy <ivymurage2000@gmail.com>
2024-09-30 10:00:20 +0200

    feat: first nest js app

---
+++ src/books/books.controller.ts
@@ -0,0 +1,30 @@
+import { Controller, Get, Post, Body, Delete, Param, Query } from '@nestjs/common';
+import { BooksService } from './books.service';
+import { CreateBookDTO } from './dto/create-book.dto';
+@Controller('books')
+export class BooksController {
+  constructor(private booksService: BooksService) {}
+
+  @Get()
+  async getBooks() {
+    const books = await this.booksService.getBooks();
+    return books;
+  }
+
+  @Get(':bookId')
+  async getBook(@Param('bookID') bookID) {
+    const book = await this.booksService.getBook(bookID);
+    return book;
+  }
+  @Post()
+  async addBooks(@Body() createBookDTO: CreateBookDTO) {
+    const book = await this.booksService.addBook(createBookDTO)
+    return book
+  }
+
+  @Delete()
+  async deleteBook(@Query() query) {
+    const books = await this.booksService.deleteBook(query.bookId)
+    return books
+  }
+}
